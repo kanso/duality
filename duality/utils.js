65662ef6e64f1732b20ef3610f5d16c028577ce2
Ryan Ramage <ryan.ramage@gmail.com>
2012-02-09 10:48:21 -0700

    Some more ways to determine baseURL
    1. Check if a baseURL is passed as a query param
    2. Check if a db and ddoc is passed as a query param
    3. check on the server side if the req.path includes a _rewrite

--- duality/utils.js
+++ duality/utils.js
@@ -125,6 +125,16 @@ exports.getBaseURL = function (/*optional*/req) {
         }
         return '';
     }
+    if (req.query.baseURL) {
+        return req.query.baseURL;
+    }
+    if (req.query.db && req.query.ddoc) {
+        return '/' + req.query.db + '/_design/' + req.query.ddoc + '/_rewrite/';
+    }
+
+    if (_.include(req.path, '_rewrite')) {
+        return '/' + req.path.slice(0, 3).join('/') + '/_rewrite';
+    }
     if (req.headers['x-couchdb-vhost-path']) {
         return '';
     }
